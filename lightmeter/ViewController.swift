61b0687bf92bdfdf43be559fe9619fb80f00b6a9
Yuto Takagi <Yuto@Yuto-MacBook-Pro.local>
2017-10-14 21:27:34 +0900

    Initial Commit

---
+++ lightmeter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lightmeter
+//
+//  Created by Yuto Takagi on 2017/10/14.
+//  Copyright © 2017年 Yuto Takagi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
